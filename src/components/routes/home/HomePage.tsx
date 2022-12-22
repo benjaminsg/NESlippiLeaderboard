f94b13c80a5faaa03674bbbf9ba43543905cc025
Benji Spetter-Goldstein <benjisg@comcast.net>
2022-12-22 08:25:50 +0000

    Bold title

--- src/components/routes/home/HomePage.tsx
+++ src/components/routes/home/HomePage.tsx
@@ -54,7 +54,7 @@ export default function HomePage() {
   return (
     <div className="flex flex-col items-center h-screen p-8 font-proximaNova">
       <h1 className="text-3xl m-4 text-center text-white font-proximaNova">
-        {settings.title}
+        <b>{settings.title}</b>
       </h1>
       <div className="p-1 text-gray-300"> Updated {updateDesc}</div>
       <Table players={players} />
