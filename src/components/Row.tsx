baf3ca264fe8b1e5561ce7b3fe06f61c54d433a3
Benji Spetter-Goldstein <benjisg@comcast.net>
2023-01-19 23:21:39 +0000

    Move rank names to hover

--- src/components/Row.tsx
+++ src/components/Row.tsx
@@ -61,17 +61,14 @@ export function Row({ player }: Props) {
         {Boolean(rankChange) && changeArrow(rankChange)} </td>
       <td className="text-gray-100 md:px-6 md:py-4 p-1 whitespace-nowrap text-center overflow-hidden md:max-w-full max-w-[7rem] text-elipses">
         <b><a className="md:text-xl text-sm max-w-xs text-gray-300 hover:text-gray-500 hover:underline" href={codeToUrlSlug(player.connectCode.code)}>{player.leaderboardName}</a></b>
-        <div className="text-gray-300 text-xs overflow-auto no-bar">{player.displayName} / {player.connectCode.code}</div>
+        <div className="text-gray-300 text-xs overflow-auto no-bar font-poyodash">{player.displayName} / {player.connectCode.code}</div>
       </td>
       <td className="md:text-xl text-sm text-gray-900 md:px-6 md:py-4 p-1 whitespace-nowrap text-center">
 
         {playerRank.iconUrl && <div className="flex items-center justify-center">
-          <img className="md:h-10 md:w-10 h-6 w-6 drop-shadow" src={playerRank.iconUrl} />
+          <img className="md:h-12 md:w-12 h-6 w-6 drop-shadow" src={playerRank.iconUrl} alt={playerRank.name} title={playerRank.name}/>
         </div>}
-        <div className="md:text-base text-xs max-w-xs text-gray-300 uppercase">
-          <b>{playerRank.name}</b>
-        </div>
-        <div className="text-gray-300 md:text-sm text-xs">
+        <div className="text-gray-300 md:text-sm text-xs font-poyodash">
           {isActive && Math.floor(player.rankedNetplayProfile.ratingOrdinal)}
           {isActive && Boolean(ratingChange) && changePlusMinus(ratingChange)}
         </div>
