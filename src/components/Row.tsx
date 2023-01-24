e5ed89f529ddb21f67b19c297d6d4395ff7a7003
Benji Spetter-Goldstein <benjisg@comcast.net>
2023-01-24 20:44:01 +0000

    Use solid background on mobile

--- src/components/Row.tsx
+++ src/components/Row.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Player } from '../lib/player'
-import { getRank } from '../lib/ranks'
+import {getRankDesktop, getRankMobile} from '../lib/ranks'
 import { Characters } from './Characters'
 import 'react-tooltip/dist/react-tooltip.css'
 import {Tooltip} from "react-tooltip";
@@ -49,42 +49,84 @@ export function Row({ player }: Props) {
     return Math.floor(player.rankedNetplayProfile.ratingOrdinal - player.oldRankedNetplayProfile.ratingOrdinal);
   }
 
-  const playerRank = getRank(player);
-  const isActive = playerRank.name !== 'None';
+  const playerRankDesktop = getRankDesktop(player);
+  const playerRankMobile = getRankMobile(player);
+  const isActive = playerRankDesktop.name !== 'None';
   const totalSets = player.rankedNetplayProfile.wins + player.rankedNetplayProfile.losses;
   const totalGames = player.rankedNetplayProfile.characters.reduce((acc, val)=> acc + val.gameCount, 0);
   const rankChange = getRankChange(player);
   const ratingChange = getRatingChange(player);
-  const rankId = playerRank.name + codeToId(player.connectCode.code)
+  const rankId = playerRankDesktop.name + codeToId(player.connectCode.code)
 
-  return (
-    <tr className={`${playerRank.bgClass} no-border`} >
-      <td className="md:text-2xl text-gray-300 md:px-6 md:py-4 md:p-1 whitespace-nowrap bottom-border">
-        <div><b>{isActive && `${player.rankedNetplayProfile.rank}`}</b></div>
-        {Boolean(rankChange) && changeArrow(rankChange)} </td>
-      <td className="text-gray-100 md:px-6 md:py-4 p-1 whitespace-nowrap text-center overflow-hidden md:max-w-full max-w-[7rem] text-elipses bottom-border">
-        <b><a className="md:text-xl text-sm max-w-xs text-gray-300 hover:text-gray-500 hover:underline" href={codeToUrlSlug(player.connectCode.code)}>{player.leaderboardName}</a></b>
-        <div className="text-gray-300 text-xs overflow-auto no-bar font-poyodash">{player.displayName} / {player.connectCode.code}</div>
-      </td>
-      <td className="md:text-xl text-sm text-gray-900 md:px-6 md:py-4 p-1 whitespace-nowrap bottom-border">
+  if (window.innerWidth > 600) {
+    return (
+        <tr className={`${playerRankDesktop.bgClass} no-border`}>
+          <td className="md:text-2xl text-gray-300 md:px-6 md:py-4 md:p-1 whitespace-nowrap bottom-border">
+            <div><b>{isActive && `${player.rankedNetplayProfile.rank}`}</b></div>
+            {Boolean(rankChange) && changeArrow(rankChange)} </td>
+          <td className="text-gray-100 md:px-6 md:py-4 p-1 whitespace-nowrap text-center overflow-hidden md:max-w-full max-w-[7rem] text-elipses bottom-border">
+            <b><a className="md:text-xl text-sm max-w-xs text-gray-300 hover:text-gray-500 hover:underline"
+                  href={codeToUrlSlug(player.connectCode.code)}>{player.leaderboardName}</a></b>
+            <div
+                className="text-gray-300 text-xs overflow-auto no-bar font-poyodash">{player.displayName} / {player.connectCode.code}</div>
+          </td>
+          <td className="md:text-xl text-sm text-gray-900 md:px-6 md:py-4 p-1 whitespace-nowrap bottom-border">
 
-        {playerRank.iconUrl && <div className="flex items-center justify-center" id={rankId} data-tooltip-content={playerRank.name}>
-          <img className="md:h-12 md:w-12 h-6 w-6 drop-shadow" src={playerRank.iconUrl}/>
-        </div>}
-          <Tooltip className="text-sm" anchorId={rankId} />
-        <div className="text-gray-300 md:text-sm text-xs font-poyodash">
-          {isActive && Math.floor(player.rankedNetplayProfile.ratingOrdinal)}
-          {isActive && Boolean(ratingChange) && changePlusMinus(ratingChange)}
-        </div>
-      </td>
-      <td className="md:text-sm text-xs text-gray-300 md:px-6 md:py-4 py-1  md:max-w-[18rem] max-w-[3rem] bottom-border">
-        <Characters player={player} totalGames={totalGames} />
-      </td>
-      <td className="md:text-xl text-gray-300 text-sm md:px-6 md:py-4 md:p-1 whitespace-nowrap overflow-auto no-bar bottom-border">
-        <b>{Boolean(totalGames) && <><span className="text-green-500">{player.rankedNetplayProfile.wins ?? 0}</span><span className="md:p-1">/</span>
-        <span className="text-red-500">{player.rankedNetplayProfile.losses ?? 0}</span>
-        </>}</b>
-      </td>
-    </tr>
-  );
+            {playerRankDesktop.iconUrl &&
+            <div className="flex items-center justify-center" id={rankId} data-tooltip-content={playerRankDesktop.name}>
+              <img className="md:h-12 md:w-12 h-6 w-6 drop-shadow" src={playerRankDesktop.iconUrl}/>
+            </div>}
+            <Tooltip className="text-sm" anchorId={rankId}/>
+            <div className="text-gray-300 md:text-sm text-xs font-poyodash">
+              {isActive && Math.floor(player.rankedNetplayProfile.ratingOrdinal)}
+              {isActive && Boolean(ratingChange) && changePlusMinus(ratingChange)}
+            </div>
+          </td>
+          <td className="md:text-sm text-xs text-gray-300 md:px-6 md:py-4 py-1  md:max-w-[18rem] max-w-[3rem] bottom-border">
+            <Characters player={player} totalGames={totalGames}/>
+          </td>
+          <td className="md:text-xl text-gray-300 text-sm md:px-6 md:py-4 md:p-1 whitespace-nowrap overflow-auto no-bar bottom-border">
+            <b>{Boolean(totalGames) && <><span className="text-green-500">{player.rankedNetplayProfile.wins ?? 0}</span><span
+                className="md:p-1">/</span>
+              <span className="text-red-500">{player.rankedNetplayProfile.losses ?? 0}</span>
+            </>}</b>
+          </td>
+        </tr>
+    );
+  } else {
+    return (
+        <tr className={`${playerRankMobile.bgClass} no-border`}>
+          <td className="md:text-2xl text-gray-300 md:px-6 md:py-4 md:p-1 whitespace-nowrap bottom-border">
+            <div><b>{isActive && `${player.rankedNetplayProfile.rank}`}</b></div>
+            {Boolean(rankChange) && changeArrow(rankChange)} </td>
+          <td className="text-gray-100 md:px-6 md:py-4 p-1 whitespace-nowrap text-center overflow-hidden md:max-w-full max-w-[7rem] text-elipses bottom-border">
+            <b><a className="md:text-xl text-sm max-w-xs text-gray-300 hover:text-gray-500 hover:underline"
+                  href={codeToUrlSlug(player.connectCode.code)}>{player.leaderboardName}</a></b>
+            <div
+                className="text-gray-300 text-xs overflow-auto no-bar font-poyodash">{player.displayName} / {player.connectCode.code}</div>
+          </td>
+          <td className="md:text-xl text-sm text-gray-900 md:px-6 md:py-4 p-1 whitespace-nowrap bottom-border">
+
+            {playerRankDesktop.iconUrl &&
+            <div className="flex items-center justify-center" id={rankId} data-tooltip-content={playerRankMobile.name}>
+              <img className="md:h-12 md:w-12 h-6 w-6 drop-shadow" src={playerRankMobile.iconUrl}/>
+            </div>}
+            <Tooltip className="text-sm" anchorId={rankId}/>
+            <div className="text-gray-300 md:text-sm text-xs font-poyodash">
+              {isActive && Math.floor(player.rankedNetplayProfile.ratingOrdinal)}
+              {isActive && Boolean(ratingChange) && changePlusMinus(ratingChange)}
+            </div>
+          </td>
+          <td className="md:text-sm text-xs text-gray-300 md:px-6 md:py-4 py-1  md:max-w-[18rem] max-w-[3rem] bottom-border">
+            <Characters player={player} totalGames={totalGames}/>
+          </td>
+          <td className="md:text-xl text-gray-300 text-sm md:px-6 md:py-4 md:p-1 whitespace-nowrap overflow-auto no-bar bottom-border">
+            <b>{Boolean(totalGames) && <><span className="text-green-500">{player.rankedNetplayProfile.wins ?? 0}</span><span
+                className="md:p-1">/</span>
+              <span className="text-red-500">{player.rankedNetplayProfile.losses ?? 0}</span>
+            </>}</b>
+          </td>
+        </tr>
+    );
+  }
 }
--- src/lib/ranks.ts
+++ src/lib/ranks.ts
@@ -28,10 +28,20 @@ interface Rank {
   iconUrl?: string
 }
 
-class NoneRank implements Rank {
+class NoneRankDesktop implements Rank {
   public name = "None"
   public iconUrl = NoneIcon
-  public bgClass = LIGHT_STONE_BG
+  public bgClass = LIGHT_STONE_GRADIENT_BG
+
+  isRank(player: Player) {
+    return setCount(player) === 0;
+  }
+}
+
+class NoneRankMobile implements Rank {
+  public name = "None"
+  public iconUrl = NoneIcon
+  public bgClass = LIGHT_STONE_SOLID_BG
 
   isRank(player: Player) {
     return setCount(player) === 0;
@@ -45,10 +55,21 @@ const setCount = (player: Player) => {
 
 const MIN_RANK_SETS = 5;
 
-class PendingRank implements Rank {
+class PendingRankDesktop implements Rank {
   public name = "Pending"
   public iconUrl = PendingIcon
-  public bgClass = DARK_STONE_BG
+  public bgClass = DARK_STONE_GRADIENT_BG
+
+  isRank(player: Player) {
+    const totalSets = setCount(player)
+    return 0 < totalSets && totalSets < MIN_RANK_SETS;
+  }
+}
+
+class PendingRankMobile implements Rank {
+  public name = "Pending"
+  public iconUrl = PendingIcon
+  public bgClass = DARK_STONE_SOLID_BG
 
   isRank(player: Player) {
     const totalSets = setCount(player)
@@ -74,9 +95,9 @@ class StandardRank implements Rank {
   }
 }
 
-class GrandMaster extends StandardRank {
+class GrandMasterDesktop extends StandardRank {
   constructor() {
-    super('Grandmaster', 2191.75, Infinity, SLATE_BG, GrandMasterIcon)
+    super('Grandmaster', 2191.75, Infinity, SLATE_GRADIENT_BG, GrandMasterIcon)
   }
 
   isRank(player: Player) {
@@ -89,44 +110,101 @@ class GrandMaster extends StandardRank {
   }
 }
 
-const ORANGE_BG = 'bg-gradient-to-r from-[#6e441a]';
-const SLATE_BG = 'bg-gradient-to-r from-gray-900';
-const YELLOW_BG = 'bg-gradient-to-r from-[#614c01]';
-const SKY_BG = 'bg-gradient-to-r from-teal-900';
-const BLUE_BG = 'bg-gradient-to-r from-[#232d94]';
-const INDIGO_BG = 'bg-gradient-to-r from-indigo-900';
-const LIGHT_STONE_BG = 'bg-gradient-to-r from-stone-500'
-const DARK_STONE_BG = 'bg-gradient-to-r from-stone-700'
+class GrandMasterMobile extends StandardRank {
+  constructor() {
+    super('Grandmaster', 2191.75, Infinity, SLATE_SOLID_BG, GrandMasterIcon)
+  }
 
-export const RANKS = [
-  new NoneRank(),
-  new PendingRank(),
-  new StandardRank('Bronze I', 0, 765.42, ORANGE_BG, Bronze1Icon),
-  new StandardRank('Bronze II', 765.43, 913.71, ORANGE_BG, Bronze2Icon),
-  new StandardRank('Bronze III', 913.72, 1054.86, ORANGE_BG, Bronze3Icon),
-  new StandardRank('Silver I', 1054.87, 1188.87, SLATE_BG, Silver1Icon),
-  new StandardRank('Silver II', 1188.88, 1315.74, SLATE_BG, Silver2Icon),
-  new StandardRank('Silver III', 1315.75, 1435.47, SLATE_BG, Silver3Icon),
-  new StandardRank('Gold I', 1435.48, 1548.06, YELLOW_BG, Gold1Icon),
-  new StandardRank('Gold II', 1548.07, 1653.51, YELLOW_BG, Gold2Icon),
-  new StandardRank('Gold III', 1653.52, 1751.82, YELLOW_BG, Gold3Icon),
-  new StandardRank('Platinum I', 1751.83, 1842.99, SKY_BG, Platinum1Icon),
-  new StandardRank('Platinum II', 1843, 1927.02, SKY_BG, Platinum2Icon),
-  new StandardRank('Platinum III', 1927.03, 2003.91, SKY_BG, Platinum3Icon),
-  new StandardRank('Diamond I', 2003.92, 2073.66, BLUE_BG, Diamond1Icon),
-  new StandardRank('Diamond II', 2073.67, 2136.27, BLUE_BG, Diamond2Icon),
-  new StandardRank('Diamond III', 2136.28, 2191.74, BLUE_BG, Diamond3Icon),
-  new StandardRank('Master I', 2191.75, 2274.99, INDIGO_BG, Master1Icon),
-  new StandardRank('Master II', 2275, 2350, INDIGO_BG, Master2Icon),
-  new StandardRank('Master III', 2350, Infinity, INDIGO_BG, Master3Icon),
-  new GrandMaster()
+  isRank(player: Player) {
+    const hasRating = super.isRank(player);
+    if(!hasRating) {
+      return false;
+    }
+    return player.rankedNetplayProfile.dailyGlobalPlacement !== null
+        || player.rankedNetplayProfile.dailyRegionalPlacement !== null;
+  }
+}
+
+const ORANGE_GRADIENT_BG = 'bg-gradient-to-r from-[#6e441a]';
+const SLATE_GRADIENT_BG = 'bg-gradient-to-r from-gray-900';
+const YELLOW_GRADIENT_BG = 'bg-gradient-to-r from-[#614c01]';
+const SKY_GRADIENT_BG = 'bg-gradient-to-r from-teal-900';
+const BLUE_GRADIENT_BG = 'bg-gradient-to-r from-[#232d94]';
+const INDIGO_GRADIENT_BG = 'bg-gradient-to-r from-indigo-900';
+const LIGHT_STONE_GRADIENT_BG = 'bg-gradient-to-r from-stone-500'
+const DARK_STONE_GRADIENT_BG = 'bg-gradient-to-r from-stone-700'
+
+const ORANGE_SOLID_BG = 'bg-orange-900';
+const SLATE_SOLID_BG = 'bg-slate-800';
+const YELLOW_SOLID_BG = 'bg-yellow-900';
+const SKY_SOLID_BG = 'bg-sky-900';
+const BLUE_SOLID_BG = 'bg-blue-900';
+const INDIGO_SOLID_BG = 'bg-indigo-900';
+const LIGHT_STONE_SOLID_BG = 'bg-stone-500'
+const DARK_STONE_SOLID_BG = 'bg-stone-700'
+
+export const RANKS_DESKTOP = [
+  new NoneRankDesktop(),
+  new PendingRankDesktop(),
+  new StandardRank('Bronze I', 0, 765.42, ORANGE_GRADIENT_BG, Bronze1Icon),
+  new StandardRank('Bronze II', 765.43, 913.71, ORANGE_GRADIENT_BG, Bronze2Icon),
+  new StandardRank('Bronze III', 913.72, 1054.86, ORANGE_GRADIENT_BG, Bronze3Icon),
+  new StandardRank('Silver I', 1054.87, 1188.87, SLATE_GRADIENT_BG, Silver1Icon),
+  new StandardRank('Silver II', 1188.88, 1315.74, SLATE_GRADIENT_BG, Silver2Icon),
+  new StandardRank('Silver III', 1315.75, 1435.47, SLATE_GRADIENT_BG, Silver3Icon),
+  new StandardRank('Gold I', 1435.48, 1548.06, YELLOW_GRADIENT_BG, Gold1Icon),
+  new StandardRank('Gold II', 1548.07, 1653.51, YELLOW_GRADIENT_BG, Gold2Icon),
+  new StandardRank('Gold III', 1653.52, 1751.82, YELLOW_GRADIENT_BG, Gold3Icon),
+  new StandardRank('Platinum I', 1751.83, 1842.99, SKY_GRADIENT_BG, Platinum1Icon),
+  new StandardRank('Platinum II', 1843, 1927.02, SKY_GRADIENT_BG, Platinum2Icon),
+  new StandardRank('Platinum III', 1927.03, 2003.91, SKY_GRADIENT_BG, Platinum3Icon),
+  new StandardRank('Diamond I', 2003.92, 2073.66, BLUE_GRADIENT_BG, Diamond1Icon),
+  new StandardRank('Diamond II', 2073.67, 2136.27, BLUE_GRADIENT_BG, Diamond2Icon),
+  new StandardRank('Diamond III', 2136.28, 2191.74, BLUE_GRADIENT_BG, Diamond3Icon),
+  new StandardRank('Master I', 2191.75, 2274.99, INDIGO_GRADIENT_BG, Master1Icon),
+  new StandardRank('Master II', 2275, 2350, INDIGO_GRADIENT_BG, Master2Icon),
+  new StandardRank('Master III', 2350, Infinity, INDIGO_GRADIENT_BG, Master3Icon),
+  new GrandMasterDesktop()
 ]
 
-export function getRank(player: Player) {
-  for(let i = RANKS.length - 1; i >= 0; i--) {
-    if(RANKS[i].isRank(player)) {
-      return RANKS[i]
+export const RANKS_MOBILE = [
+  new NoneRankMobile(),
+  new PendingRankMobile(),
+  new StandardRank('Bronze I', 0, 765.42, ORANGE_SOLID_BG, Bronze1Icon),
+  new StandardRank('Bronze II', 765.43, 913.71, ORANGE_SOLID_BG, Bronze2Icon),
+  new StandardRank('Bronze III', 913.72, 1054.86, ORANGE_SOLID_BG, Bronze3Icon),
+  new StandardRank('Silver I', 1054.87, 1188.87, SLATE_SOLID_BG, Silver1Icon),
+  new StandardRank('Silver II', 1188.88, 1315.74, SLATE_SOLID_BG, Silver2Icon),
+  new StandardRank('Silver III', 1315.75, 1435.47, SLATE_SOLID_BG, Silver3Icon),
+  new StandardRank('Gold I', 1435.48, 1548.06, YELLOW_SOLID_BG, Gold1Icon),
+  new StandardRank('Gold II', 1548.07, 1653.51, YELLOW_SOLID_BG, Gold2Icon),
+  new StandardRank('Gold III', 1653.52, 1751.82, YELLOW_SOLID_BG, Gold3Icon),
+  new StandardRank('Platinum I', 1751.83, 1842.99, SKY_SOLID_BG, Platinum1Icon),
+  new StandardRank('Platinum II', 1843, 1927.02, SKY_SOLID_BG, Platinum2Icon),
+  new StandardRank('Platinum III', 1927.03, 2003.91, SKY_SOLID_BG, Platinum3Icon),
+  new StandardRank('Diamond I', 2003.92, 2073.66, BLUE_SOLID_BG, Diamond1Icon),
+  new StandardRank('Diamond II', 2073.67, 2136.27, BLUE_SOLID_BG, Diamond2Icon),
+  new StandardRank('Diamond III', 2136.28, 2191.74, BLUE_SOLID_BG, Diamond3Icon),
+  new StandardRank('Master I', 2191.75, 2274.99, INDIGO_SOLID_BG, Master1Icon),
+  new StandardRank('Master II', 2275, 2350, INDIGO_SOLID_BG, Master2Icon),
+  new StandardRank('Master III', 2350, Infinity, INDIGO_SOLID_BG, Master3Icon),
+  new GrandMasterMobile()
+]
+
+export function getRankDesktop(player: Player) {
+  for(let i = RANKS_DESKTOP.length - 1; i >= 0; i--) {
+    if(RANKS_DESKTOP[i].isRank(player)) {
+      return RANKS_DESKTOP[i]
     }
   }
-  return new NoneRank()
+  return new NoneRankDesktop()
+}
+
+export function getRankMobile(player: Player) {
+  for(let i = RANKS_MOBILE.length - 1; i >= 0; i--) {
+    if(RANKS_MOBILE[i].isRank(player)) {
+      return RANKS_MOBILE[i]
+    }
+  }
+  return new NoneRankMobile()
 }
