f06ffef14ae9543a62a3ce122afc1da1dd27f12f
Benji Spetter-Goldstein <benjisg@comcast.net>
2025-04-16 14:46:23 +0000

    Update API call

--- cron/slippi.js
+++ cron/slippi.js
@@ -14,7 +14,7 @@ var __generator = (this && this.__generator) || function (thisArg, body) {
     function verb(n) { return function (v) { return step([n, v]); }; }
     function step(op) {
         if (f) throw new TypeError("Generator is already executing.");
-        while (g && (g = 0, op[0] && (_ = 0)), _) try {
+        while (_) try {
             if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
             if (y = 0, t) op = [op[0] & 2, t.value];
             switch (op[0]) {
@@ -43,8 +43,8 @@ var getPlayerData = function (connectCode) { return __awaiter(void 0, void 0, vo
     return __generator(this, function (_a) {
         switch (_a.label) {
             case 0:
-                query = "fragment userProfilePage on User {\n    displayName\n    connectCode {\n          code\n          __typename\n        }\n      rankedNetplayProfile {\n            id\n            ratingOrdinal\n            ratingUpdateCount\n            wins\n            losses\n            dailyGlobalPlacement\n            dailyRegionalPlacement\n            continent\n            characters {\n                    id\n                    character\n                    gameCount\n                    __typename\n                  }\n            __typename\n          }\n      __typename\n  }\n\n  query AccountManagementPageQuery($cc: String!) {\n      getConnectCode(code: $cc) {\n            user {\n                    ...userProfilePage\n                    __typename\n                  }\n            __typename\n          }\n  }";
-                return [4 /*yield*/, fetch('https://gql-gateway-dot-slippi.uc.r.appspot.com/graphql', {
+                query = "fragment profileFieldsV2 on NetplayProfileV2 {\n    id\n    ratingOrdinal\n    ratingUpdateCount\n    wins\n    losses\n    dailyGlobalPlacement\n    dailyRegionalPlacement\n    continent\n    characters {\n      character\n      gameCount\n      __typename\n    }\n    __typename\n  }\n\n  fragment userProfilePage on User {\n    fbUid\n    displayName\n    connectCode {\n      code\n      __typename\n    }\n    rankedNetplayProfile {\n      ...profileFieldsV2\n      __typename\n    }\n    __typename\n  }\n    \n  query AccountManagementPageQuery($cc: String!) {\n    getConnectCode(code: $cc) {\n      user {\n        ...userProfilePage\n        __typename\n      }\n      __typename\n    }\n  }";
+                return [4 /*yield*/, fetch('https://gql-gateway-2-dot-slippi.uc.r.appspot.com/graphql', {
                         headers: {
                             'content-type': 'application/json'
                         },
